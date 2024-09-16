c2c3d405b2d44c60690ce2b287148c9cc1035cb9
Leandro Motta Barros <lmb@stackedboxes.org>
2024-09-16 15:10:21 -0300

    Add a 128x64 display, show some random dots

--- temperature-humidity-sensor/go.mod
+++ temperature-humidity-sensor/go.mod
@@ -3,3 +3,5 @@ module main
 go 1.23.0
 
 require tinygo.org/x/drivers v0.28.0
+
+require github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
--- temperature-humidity-sensor/main.go
+++ temperature-humidity-sensor/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"machine"
+	"math/rand"
 	"time"
 
 	"tinygo.org/x/drivers/dht"
+	"tinygo.org/x/drivers/ssd1306"
 )
 
 func main() {
@@ -14,8 +17,28 @@ func main() {
 
 	dht11 := dht.New(machine.GPIO15, dht.DHT11)
 
+	displayI2C := machine.I2C0
+	err := displayI2C.Configure(machine.I2CConfig{
+		SCL:       machine.GPIO1,
+		SDA:       machine.GPIO0,
+		Frequency: 400 * machine.KHz,
+	})
+	if err != nil {
+		println("could not configure I2C:", err)
+		return
+	}
+
+	display := ssd1306.NewI2C(displayI2C)
+	display.Configure(ssd1306.Config{
+		Width:    128,
+		Height:   64,
+		Address:  0x3C,
+		VccState: ssd1306.SWITCHCAPVCC,
+	})
+	display.ClearDisplay()
+
 	for {
-		// Blink
+		// Blink LED
 		led.High()
 		time.Sleep(time.Second / 3)
 
@@ -24,7 +47,7 @@ func main() {
 
 		// Read sensor
 		temperature, err := dht11.TemperatureFloat(dht.C)
-		if err == nil {
+		if err != nil {
 			fmt.Printf("Error reading temperature: %v", err)
 		}
 
@@ -34,5 +57,16 @@ func main() {
 		}
 
 		fmt.Printf("Temperature: %.1f°C\nHumidity: %.1f%%\n\n", temperature, humidity)
+
+		display.ClearBuffer()
+		for x := int16(0); x < 128; x++ {
+			for y := int16(0); y < 64; y++ {
+				p := (float32(x) / 128) * (float32(y) / 64)
+				if p < rand.Float32() {
+					display.SetPixel(x, y, color.RGBA{255, 255, 255, 255})
+				}
+			}
+		}
+		display.Display()
 	}
 }
