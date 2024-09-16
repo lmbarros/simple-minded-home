0cb521071576dd904bffb8c7af0aea56179b2b17
Leandro Motta Barros <lmb@stackedboxes.org>
2024-09-16 11:43:18 -0300

    Read temperature and humidity from a DHT11 sensor
    Very nice first experience with TinyGo and its drivers.

--- temperature-humidity-sensor/go.mod
+++ temperature-humidity-sensor/go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.23.0
+
+require tinygo.org/x/drivers v0.28.0
--- temperature-humidity-sensor/main.go
+++ temperature-humidity-sensor/main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"machine"
 	"time"
+
+	"tinygo.org/x/drivers/dht"
 )
 
 func main() {
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	dht11 := dht.New(machine.GPIO15, dht.DHT11)
+
 	for {
+		// Blink
 		led.High()
 		time.Sleep(time.Second / 3)
 
 		led.Low()
 		time.Sleep(time.Second * 2)
+
+		// Read sensor
+		temperature, err := dht11.TemperatureFloat(dht.C)
+		if err == nil {
+			fmt.Printf("Error reading temperature: %v", err)
+		}
+
+		humidity, err := dht11.HumidityFloat()
+		if err != nil {
+			fmt.Printf("Error reading humidity: %v", err)
+		}
+
+		fmt.Printf("Temperature: %.1f°C\nHumidity: %.1f%%\n\n", temperature, humidity)
 	}
 }
